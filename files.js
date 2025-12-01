c73301934ecd166df5a5468e417374665bd5373c
mo7amade8-afk <mo7amade8@gmail.com>
2025-12-01 22:14:45 +0100

    Create files.js

---
+++ files.js
@@ -0,0 +1,17 @@
+
+// files.js
+
+module.exports = [
+  {
+    command: "note",
+    path: "/texts/note.txt"
+  },
+  {
+    command: "readme",
+    path: "/texts/info.txt"
+  },
+  {
+    command: "apk",
+    path: "/files/app.apk"
+  }
+];
