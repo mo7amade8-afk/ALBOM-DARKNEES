a66f8e4a189f86b83137ac5c5fb86acfa50b77e0
mo7amade8-afk <mo7amade8@gmail.com>
2025-12-01 22:09:23 +0100

    Create images.js

---
+++ images.js
@@ -0,0 +1,17 @@
+
+// images.js
+
+module.exports = [
+  {
+    command: "cat",
+    path: "/images/cat.png"
+  },
+  {
+    command: "dog",
+    path: "/images/dog.jpg"
+  },
+  {
+    command: "logo",
+    path: "/images/logo.png"
+  }
+];
