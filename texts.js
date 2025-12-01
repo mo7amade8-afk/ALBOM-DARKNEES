c17de3c37fcf1f23a8f339619540c7fbf1e33195
mo7amade8-afk <mo7amade8@gmail.com>
2025-12-01 22:18:54 +0100

    Create texts.js

---
+++ texts.js
@@ -0,0 +1,12 @@
+
+// texts.js
+module.exports = [
+  {
+    command: "welcome",
+    text: "Hello! Welcome to my bot."
+  },
+  {
+    command: "info",
+    text: "This bot sends images, videos, files, and texts."
+  }
+];
