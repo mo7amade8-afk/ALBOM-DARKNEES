6b3e796115381bed32ab9ca8fff3b1aec6177cfd
mo7amade8-afk <mo7amade8@gmail.com>
2025-12-02 12:33:18 +0100

    Update and rename files.js to file.js

---
+++ file.js
@@ -0,0 +1,41 @@
+
+// files.js
+
+module.exports = [
+  {
+    command: "note",
+    path: "/texts/note.txt"
+  },
+  {
+    command: "readme",
+    path: "/texts/info.txt"
+  },
+  {
+    command: "apk",
+    path: "/files/app.apk"
+  }
+];
+file.js
+
+export default {
+  "ـ👑ـpdf": "https://raw.githubusercontent.com/user/repo/main/file.pdf",
+  "ـ👑ـdoc": "https://raw.githubusercontent.com/user/repo/main/doc.docx"
+};
+file.js
+
+export default {
+  "ـ👑ـpdf": "https://raw.githubusercontent.com/user/repo/main/file.pdf",
+  "ـ👑ـdoc": "https://raw.githubusercontent.com/user/repo/main/doc.docx"
+};
+file.js
+
+export default {
+  "ـ👑ـpdf": "https://raw.githubusercontent.com/user/repo/main/file.pdf",
+  "ـ👑ـdoc": "https://raw.githubusercontent.com/user/repo/main/doc.docx"
+};
+file.js
+
+export default {
+  "ـ👑ـpdf": "https://raw.githubusercontent.com/user/repo/main/file.pdf",
+  "ـ👑ـdoc": "https://raw.githubusercontent.com/user/repo/main/doc.docx"
+};
