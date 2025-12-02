2d45c11b7a60cab0e3b5abd1ae3eebc4422bd354
mo7amade8-afk <mo7amade8@gmail.com>
2025-12-02 12:51:25 +0100

    Delete images.js

--- images.js
+++
@@ -1,6 +0,0 @@
-
-export default {
-  "جلجامشة": "https://raw.githubusercontent.com/USERNAME/REPO/main/images/جلجامشة.jpg",
-  "ـ👑ـ": "https://raw.githubusercontent.com/USERNAME/REPO/main/images/example2.png",
-  "ـ👑ـ": "https://raw.githubusercontent.com/USERNAME/REPO/main/images/example3.jpeg"
-};
