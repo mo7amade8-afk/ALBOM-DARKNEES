e9ffc9556b20677ca6c4f7bf6688c7365d564ab9
mo7amade8-afk <mo7amade8@gmail.com>
2025-12-02 10:11:40 +0100

    Update images.js

--- images.js
+++ images.js
@@ -1,6 +1,6 @@
 
 export default {
-  "ـ👑ـ": "https://raw.githubusercontent.com/USERNAME/REPO/main/images/example1.jpg",
+  "جلجامشة": "https://raw.githubusercontent.com/USERNAME/REPO/main/images/جلجامشة.jpg",
   "ـ👑ـ": "https://raw.githubusercontent.com/USERNAME/REPO/main/images/example2.png",
   "ـ👑ـ": "https://raw.githubusercontent.com/USERNAME/REPO/main/images/example3.jpeg"
 };
